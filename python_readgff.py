bbc3d4df23365f09bcfb2d34f1c6b0f06ffde74b
franmorena <cielly.morena.fm@gmail.com>
2023-04-12 09:42:59 -0500

    csv module imported

--- python_readgff.py
+++ python_readgff.py
@@ -2,6 +2,7 @@
 
 # import modules
 import argparse
+import csv 
 
 # create an ArgumentParser Object 
 parser = argparse.ArgumentParser(description="this script prints SBASH script with passed parameters/argument and options")
@@ -17,10 +18,10 @@ args = parser.parse_args()
 # commands here 
 
 # open files and read lines - line by line
-gff_file = open(args.gff_file, "r")
-gff_lines = gff_file.readlines()
-gff_lines.remove(gff_lines[-1])
-gff_file.close()
+# gff_file = open(args.gff_file, "r")
+# gff_lines = gff_file.readlines()
+# gff_lines.remove(gff_lines[-1])
+# gff_file.close()
  
 # loop over all the lines in the file with for loop - remove `#` if for loop is desired
 # for line in gff_lines: 
@@ -33,37 +34,41 @@ gff_file.close()
 # open the gff file
 with open(args.gff_file) as gff:
 
+	# create a csv reader object
+	reader = csv.reader(gff, delimiter='\t')
+	
 	# loop over all the lines in the file with `with`
-	for line in gff:
+	for line in reader:
 	
 		# skip blank line
-		if not line.strip():
+		if not line:
 			continue
 			
 		# else it's not blank
-		else: 
-			line = line.rstrip() # rstrip removes the blank space on the right (beginning of the line) 
+		else: #you don't need the else if you use csv bc it strips it for you, you can leave it here for aesthetics/readability, it won't hurt the script
+			# line = line.rstrip() # rstrip removes the blank space on the right (beginning of the line) 
 								# and .lstrip removes the blank space on the left (end of the line)
 			
 		# split line on tab character
-			columns = line.split('\t') 
+			# columns = line.split('\t') 
 	
-		#give variables names to the columns 
-			organism = columns[0]
-			source = columns[1]
-			feature_type = columns[2]
-			start = int(columns[3])
-			end = int(columns[4])
-			length = columns[5]
-			strand = columns[6]
-			attributes = columns[8]
+		#give variables names to the line variable | it was column before  
+			organism = line[0]
+			source = line[1]
+			feature_type = line[2]
+			start = int(line[3])
+			end = int(line[4])
+			
+			# add the length to column 5
+			line[5] = str(end - start +1)
+			
+			length = line[5]
+			strand = line[6]
+			attributes = line[8]
 		
-		# add the length to column 5
-			columns[5] = str(end - start +1)
 		
 		# joint in a character 
-			new_line = '\t'.join(columns)
-		
+			new_line = '\t'.join(line)
 			print(new_line)
 
 
